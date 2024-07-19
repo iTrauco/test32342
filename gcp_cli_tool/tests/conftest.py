c45675dcc5457b581cb24978a4dd537b9da09e38
itrauco <chris@trau.co>
2024-07-18 20:40:12 -0400

    Reworking cli_setup.py setup script to include .gitignore and add pytest.ini in root level of dev directory

--- gcp_cli_tool/tests/conftest.py
+++ gcp_cli_tool/tests/conftest.py
@@ -1,1 +1,5 @@
-# Configuration file for pytest, if needed
+import sys
+import os
+
+# Add the gcp_cli_tool directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../gcp-cli-tool')))
